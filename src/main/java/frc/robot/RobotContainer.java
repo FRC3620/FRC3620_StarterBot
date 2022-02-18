3376ae27500bd48d460b010ccbbcaa5d750eea2b
Doug Wegscheid <dwegscheid@sbcglobal.net>
2022-02-18 16:48:53 -0500

    Flesh out RobotContainer.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,16 +4,20 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.slf4j.Logger;
 import org.usfirst.frc3620.logger.EventLogging;
 import org.usfirst.frc3620.logger.EventLogging.Level;
 import org.usfirst.frc3620.misc.CANDeviceFinder;
 
-import edu.wpi.first.wpilibj.GenericHID;
-import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import org.usfirst.frc3620.misc.CANDeviceType;
+import org.usfirst.frc3620.misc.RobotParameters;
+import org.usfirst.frc3620.misc.RobotParametersContainer;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -26,19 +30,57 @@ public class RobotContainer {
   
   // need this
   static CANDeviceFinder canDeviceFinder;
+  static RobotParameters robotParameters;
 
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  // hardware here...
+  private static DigitalInput practiceBotJumper;
 
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+  // subsystems here
+  private static ExampleSubsystem exampleSubsystem;
+
+  // joysticks here....
+  public static Joystick driverJoystick;
+  public static Joystick operatorJoystick;
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     canDeviceFinder = new CANDeviceFinder();
     logger.info ("CAN bus: " + canDeviceFinder.getDeviceSet());
 
+    robotParameters = RobotParametersContainer.getRobotParameters(RobotParameters.class);
+    logger.info ("got parameters for chassis '{}'", robotParameters.getName());
+
+    makeHardware();
+    setupMotors();
+    makeSubsystems();
     // Configure the button bindings
     configureButtonBindings();
+    setupSmartDashboardCommands();
+    setupAutonomousCommands();
+  }
+
+  void makeHardware() {
+    practiceBotJumper = new DigitalInput(0);
+    boolean iAmACompetitionRobot = amIACompBot();
+    if (!iAmACompetitionRobot) {
+      logger.warn("this is a test chassis, will try to deal with missing hardware!");
+    }
+
+    PneumaticsModuleType pneumaticModuleType = null;
+
+    if (canDeviceFinder.isDevicePresent(CANDeviceType.REV_PH, 1, "REV PH") || iAmACompetitionRobot) {
+      pneumaticModuleType = PneumaticsModuleType.REVPH;
+    } else if (canDeviceFinder.isDevicePresent(CANDeviceType.CTRE_PCM, 0, "CTRE PCM")) {
+      pneumaticModuleType = PneumaticsModuleType.CTREPCM;
+    }
+  }
+
+  private void setupMotors() {
+
+  }
+
+  private void makeSubsystems() {
+    exampleSubsystem = new ExampleSubsystem();
   }
 
   /**
@@ -47,7 +89,21 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+    driverJoystick = new Joystick(0);
+    operatorJoystick = new Joystick(1);
+  }
+
+  private void setupSmartDashboardCommands() {
+    // SmartDashboard.putData(new xxxxCommand());
+  }
+
+  SendableChooser<Command> chooser = new SendableChooser<>();
+  public void setupAutonomousCommands() {
+    SmartDashboard.putData("Auto mode", chooser);
+
+    chooser.addOption("Example Command", new ExampleCommand(exampleSubsystem));
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
@@ -56,6 +112,35 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
-    return m_autoCommand;
+    //return new GoldenAutoCommand(driveSubsystem, shooterSubsystem, VisionSubsystem, intakeSubsystem);
+    return chooser.getSelected();
   }
+
+  /**
+   * Determine if this robot is a competition robot.
+   *
+   * It is if it's connected to an FMS.
+   *
+   * It is if it is missing a grounding jumper on DigitalInput 0.
+   *
+   * It is if the robot_parameters.json says so for this MAC address.
+   *
+   * @return true if this robot is a competition robot.
+   */
+  public static boolean amIACompBot() {
+    if (DriverStation.isFMSAttached()) {
+      return true;
+    }
+
+    if(practiceBotJumper.get() == true){
+      return true;
+    }
+
+    if (robotParameters.isCompetitionRobot()) {
+      return true;
+    }
+
+    return false;
+  }
+
 }
